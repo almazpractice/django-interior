7fd8bd140cf4e214764a4db65af986bd27050d72
Алмаз Гимаев <almaz101991@yandex.ru>
2021-12-04 19:41:13 +0300

    search feature added

--- interior/main/views.py
+++ interior/main/views.py
@@ -3,12 +3,21 @@ from django.shortcuts import get_object_or_404, render
 from .models import Card, Group, Product
 
 
+def searching(request, keyword):
+    products = Product.objects.filter(title__contains=keyword)
+    return render(request, 'main/catalog.html', {'products': products, 'keyword': keyword})
+
 def index(request):
     offers = Product.objects.filter(old_price__gte=1)[:3]
     products = Product.objects.all()[:3]
+    keyword = request.GET.get("q", None)
+    if keyword:
+        products = Product.objects.filter(title__contains=keyword)
+        return render(request, 'main/catalog.html', {'products': products, 'keyword': keyword})
     context = {
         'offers': offers,
         'products': products,
+        'keyword': keyword
     }
     return render(request, 'main/index.html', context)
 
@@ -16,9 +25,14 @@ def index(request):
 def catalog(request):
     offers = Product.objects.filter(old_price__gte=1)
     products = Product.objects.all().order_by('price')
+    keyword = request.GET.get("q", None)
+    if keyword:
+        products = Product.objects.filter(title__contains=keyword)
+        return render(request, 'main/catalog.html', {'products': products, 'keyword': keyword})
     context = {
         'offers': offers,
         'products': products,
+        'keyword': keyword
     }
     return render(request, 'main/catalog.html', context)
 
@@ -26,9 +40,14 @@ def catalog(request):
 def group_list(request, slug):
     group = get_object_or_404(Group, slug=slug)
     products = group.products.all()
+    keyword = request.GET.get("q", None)
+    if keyword:
+        products = Product.objects.filter(title__contains=keyword)
+        return render(request, 'main/catalog.html', {'products': products, 'keyword': keyword})
     context = {
         'group': group,
         'products': products,
+        'keyword': keyword
     }
     return render(request, 'main/group.html', context)
 
@@ -37,8 +56,13 @@ def group_list(request, slug):
 def cart(request):
     products = Product.objects.all()[:10]
     cards = Card.objects.all()
+    keyword = request.GET.get("q", None)
+    if keyword:
+        products = Product.objects.filter(title__contains=keyword)
+        return render(request, 'main/catalog.html', {'products': products, 'keyword': keyword})
     context = {
         'products': products,
         'cards': cards,
+        'keyword': keyword
     }
     return render(request, 'main/cart.html', context)
